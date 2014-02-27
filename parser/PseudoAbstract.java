c809bcb5c126cd8cb47c6fbc5a86c103db8840b1
NoeHedde <noe.hedde@gmail.com>
2014-02-27 15:49:50 +0100

    Create PseudoAbstract.java

---
+++ parser/PseudoAbstract.java
@@ -0,0 +1,22 @@
+package parser;
+
+public class PseudoAbstract {
+        private String name;
+        private int id;
+        
+        public void setName(String s) {
+                this.name = s;
+        }
+        
+        public string getName() {
+                return this.name;
+        }
+        
+        public void setId(int n) {
+                this.id = n;
+        }
+        
+        public int getId() {
+                return this.id;
+        }
+}
