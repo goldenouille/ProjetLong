b1703969895b4bc5c432507a32f841850062fe10
goldenouille <gbtravian@free.fr>
2014-02-27 16:10:39 +0100

    Update LinkKeyWordToUMLStep.java
    creation de getCorrection

--- model/LinkKeyWordToUMLStep.java
+++ model/LinkKeyWordToUMLStep.java
@@ -1,3 +1,25 @@
+package model;
+
+import java.util.ArrayList;
+
+import controller.ModelController;
+
 public class LinkKeyWordToUMLStep extends Step {
+
+	public LinkKeyWordToUMLStep() {
+		this.name = "LinkKeyWordToUMLStep";
+		this.frenchName = "Association des mots-clef avec un type UML";
+	}
 	
+	public void getCorrection(Exercise exo) {
+		ArrayList<Word> text = exo.getText();
+		ArrayList<Word> userText = exo.getUserText();
+		ModelController mc = exo.getModelController();
+
+		for (int i = 0; i < text.size(); i++) {
+			if (text.get(i).isSelected()) {
+				mc.UMLtypeCorrect(i,i,i.umlNature==i.userUmlNature);
+			}
+		}
+	}		
 }
