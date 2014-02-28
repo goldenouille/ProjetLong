9942698b84ecc4214f854f8104287e837aa346d0
NoeHedde <noe.hedde@gmail.com>
2014-02-28 15:05:33 +0100

    Create PseudoHeritage.java

---
+++ parser/PseudoHeritage.java
@@ -0,0 +1,9 @@
+package parser;
+
+public class PseudoHeritage extends PseudoBiEdge{
+
+        public PseudoHeritage() {
+                super();
+                this.frenchName = "Heritage";
+        }
+}
