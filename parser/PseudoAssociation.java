30d029facb7e17b144a743fadd582ccdb9fe6046
NoeHedde <noe.hedde@gmail.com>
2014-02-28 15:02:32 +0100

    Create PseudoAssociation.java

---
+++ parser/PseudoAssociation.java
@@ -0,0 +1,9 @@
+package parser;
+
+public class PseudoAssociation extends PseudoBiEdge{
+
+        public PseudoAssociation() {
+                super();
+                this.frenchName = "Association";
+        }
+}
