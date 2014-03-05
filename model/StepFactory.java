53080d70c6b26d290098a30b2a6b320d8fdd18c6
Alice TAILLIAR <alice.tailliar@etu.enseeiht.fr>
2014-03-05 18:00:00 +0100

    ajout de method de static dans StepFactory

--- model/StepFactory.java
+++ model/StepFactory.java
@@ -2,9 +2,12 @@ package model;
 
 public class StepFactory {
 
-	public Step createStep(String name) {
-		if (name.equals("selectKeyWord")) {
+	public static Step createStep(String name) {
+		if (name.equalsIgnoreCase("selectKeyWord")) {
 			return new SelectKeyWordStep();
+		}
+		if (name.equalsIgnoreCase("LinkKeyWordToUML")) {
+			return new LinkKeyWordToUMLStep();
 		}
 
  		return new SelectKeyWordStep();
